f41878e732e21140c1b8f8b7d6ad59f8e6bddf03
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-06-14 12:13:10 +0300

    still hoping

---
+++ client/build/precache-manifest.1fb971fb382319f68e9e05c9514b926a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a624ef22f8d50a5ada3ac3f500c0a49e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f944d408815c761e9b8",
+    "url": "/static/css/main.06f023c9.chunk.css"
+  },
+  {
+    "revision": "aa0ecbf0a84eea97f955",
+    "url": "/static/js/2.07b05656.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07b05656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f944d408815c761e9b8",
+    "url": "/static/js/main.7f2782a6.chunk.js"
+  },
+  {
+    "revision": "acfdd1fc308ac6aa2560",
+    "url": "/static/js/runtime-main.1ae1de7a.js"
+  }
+]);
