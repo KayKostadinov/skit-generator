ff17f824afb97ad3abfe3b9bcc6bb850e2ba1cb4
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-08-26 21:02:02 +0300

    try deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -57,6 +57,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
 })
